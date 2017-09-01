beac10a97b8b390ea9bce9e0fcc567533cb88d34
Balint Cristian <cristian.balint@gmail.com>
2017-09-01 15:34:05 +0300

    * Fix c++11 requirement. * Enable LAB in our small sample script.

--- samples/1-segment.sh
+++ samples/1-segment.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 rm -rf output-slic.???
-../bin/gdal-segment -algo SLIC -region 10 -niter 10 25cm_orto0.jpg -out output-slic.shp
+../bin/gdal-segment -algo SLIC -region 10 -niter 10 25cm_orto0.jpg -lab -out output-slic.shp
 
 rm -rf output-slico.???
-../bin/gdal-segment -algo SLICO -region 10 -niter 10 25cm_orto0.jpg -out output-slico.shp
+../bin/gdal-segment -algo SLICO -region 10 -niter 10 25cm_orto0.jpg -lab -out output-slico.shp
 
 rm -rf output-mslic.???
-../bin/gdal-segment -blur -algo MSLIC -region 15 -niter 10 25cm_orto0.jpg -out output-mslic.shp
+../bin/gdal-segment -blur -algo MSLIC -region 15 -niter 10 25cm_orto0.jpg -lab -out output-mslic.shp
 
 rm -rf output-seeds.???
-../bin/gdal-segment -algo SEEDS -region 10 -niter 25 25cm_orto0.jpg -out output-seeds.shp
+../bin/gdal-segment -algo SEEDS -region 10 -niter 25 25cm_orto0.jpg -lab -out output-seeds.shp
 
 rm -rf output-lsc.???
-../bin/gdal-segment -algo LSC -region 10 -niter 20 25cm_orto0.jpg -out output-lsc.shp
+../bin/gdal-segment -algo LSC -region 10 -niter 20 25cm_orto0.jpg -lab -out output-lsc.shp
